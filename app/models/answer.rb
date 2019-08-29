1ce1f6389177a7ef5459e962e5815b8271704bf3
JinnFighter <chernayamolnia@yandex.ru>
2019-08-29 09:40:05 +0300

    Best answer now appears on top of other answers.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,8 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :user
 
+  default_scope { order(best_answer: :desc) }
+
   def set_best!
     question.answers.update_all(best_answer: false)
     update!(best_answer: true)
