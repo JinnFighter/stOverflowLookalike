f87b161871ecb3e430b911b9acad1a10ce3071bf
JinnFighter <chernayamolnia@yandex.ru>
2019-08-29 10:10:10 +0300

    Fixed edit someone elses answer spec in answers_controller_spec.

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -117,23 +117,23 @@ RSpec.describe AnswersController, type: :controller do
 
       context 'edit someone elses answer' do
         it 'assigns the requested answer to @answer' do
-          patch :update, params: { id: answer, answer: attributes_for(:answer), format: :js }
-          expect(assigns(:answer)).to eq(answer)
+          patch :update, params: { id: other_answer, answer: attributes_for(:answer), format: :js }
+          expect(assigns(:answer)).to eq(other_answer)
         end
 
         it 'assigns the question' do
-          patch :update, params: { id: answer, question_id: question, answer: { body: "new answer" }, format: :js }
+          patch :update, params: { id: other_answer, question_id: question, answer: { body: "new answer" }, format: :js }
           expect(assigns(:question)).to eq question
         end
 
-        it 'changes answer attributes' do
-          patch :update, params: { id: answer, question_id: question, answer: { body: "new answer" }, format: :js }
+        it 'does not change answer attributes' do
+          patch :update, params: { id: other_answer, question_id: question, answer: { body: "new answer" }, format: :js }
           answer.reload
-          expect(answer.body).to eq "new answer"
+          expect(other_answer.body).to eq "MyText"
         end
 
         it 'renders update template' do
-          patch :update, params: { id: answer, question_id: question, answer: attributes_for(:answer), format: :js }
+          patch :update, params: { id: other_answer, question_id: question, answer: attributes_for(:answer), format: :js }
           expect(response).to render_template :update
         end
       end
