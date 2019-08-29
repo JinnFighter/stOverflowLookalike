7c52c289d1f9f44c0da88847c13b334b75fadd4e
JinnFighter <chernayamolnia@yandex.ru>
2019-08-29 10:03:39 +0300

    Added model spec for setting best answer and fixed factory.

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :answer do
     body "MyText"
     user
+    best_answer false
 
     factory :answer_to_question do
       body "Test_body"
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -3,5 +3,27 @@ require 'rails_helper'
 RSpec.describe Answer, type: :model do
   it { should validate_presence_of(:body) }
   it { should belong_to(:question) }
+  it { should belong_to(:user) }
 
+  describe 'sets best_answer' do
+    let!(:user) { create(:user) }
+    let!(:question) { create :question, user: user }
+    let!(:answer) { create :answer, question: question }
+    let!(:other_answer) { create :answer, question: question, best_answer: true }
+
+    it 'should set answer as best' do
+      answer.set_best!
+      expect(answer.best_answer).to be true
+    end
+
+    it 'should make other answers not best' do
+      answer.set_best!
+
+      answer.reload
+      other_answer.reload
+
+      expect(answer.best_answer).to be true
+      expect(other_answer.best_answer).to be false
+    end
+  end
 end
